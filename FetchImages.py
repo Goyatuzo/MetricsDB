100609b8d5916c7c96756782cab0513ec189e3cf
Yuto Otaguro <yotagur@gmail.com>
2015-07-21 12:04:10 -0500

    Yeah forgot having 200 threads was a bad idea.

--- FetchImages.py
+++ FetchImages.py
@@ -23,27 +23,54 @@ threads = []
 results = []
 
 def getImage(name, imgOne, imgTwo):
-	print '\t' + name
-
 	# Make sure that imgOne or imgTwo are not null..
 	if imgOne:
-		results.append(urllib.urlretrieve(imgOne, dirname + name + "-1.png"))
-	if imgTwo:	
-		results.append(urllib.urlretrieve(imgTwo, dirname + name + "-2.png"))
+		# There may be threading errors. Hopefully not anymore.
+		try:
+			results.append(urllib.urlretrieve(imgOne, dirname + name + "-1.png"))
+			# Check to see an image was downloaded. If not, download medium instead.
+			if os.stat(dirname + name + "-1.png").st_size == 0:
+				print "\t" + "IMG 1: Large for " + name + " does not exist. Downloading medium."
+				# Replace 960,960 with 480,480
+				medOne = imgOne.replace("960,960", "480,480")
+				results.append(urllib.urlretrieve(medOne, dirname + name + "-1.png"))
+		except:
+			print "\t\tIMG 1: " + name + " has an error."
+	# Make sure the image exists.
+	if imgTwo:
+		# There may be threading errors. Hopefully not anymore.
+		try:
+			results.append(urllib.urlretrieve(imgTwo, dirname + name + "-2.png"))
+			if os.stat(dirname + name + "-2.png").st_size == 0:
+				print "\t" +"IMG 2: Large for " + name + " does not exist. Downloading medium."
+				# Replace 960,960 with 480,480
+				medTwo = imgTwo.replace("960,960", "480,480")
+				results.append(urllib.urlretrieve(medTwo, dirname + name + "-2.png"))
+		except:
+			print "\t\tIMG 2: " + name + " has an error."
+		
 
 # Keep counter to print out name every 10 images.
 for image in cursor:
+	while threading.active_count() > 10:
+		time.sleep(1)
+
 	# Set each query item a variable name to be called into the thread.
 	name = image[0].lower().replace(' ', '-')
 	imgOne = image[1]
 	imgTwo = image[2]
+
+	print name
 
 	# Retrieve each image through a thread.
 	t = threading.Thread(target = getImage, args = (name, imgOne, imgTwo))
 	t.start()
 	threads.append(t)
 	# Wait one second or else too many files will be open.
-	time.sleep(0.1)
+	time.sleep(0.5)
+
+#while threading.active_count > 0:
+#	time.sleep(0.2)
 
 map(lambda t: t.join(), threads)
 
